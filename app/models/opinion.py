97934f1285c541b5f5723424c26f2406cb596159
jarkol01 <s221485@student.uek.krakow.pl>
2022-06-10 11:32:37 +0200

    HOMEWORK JK

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,3 +1,13 @@
 class Opinion():
-    def __init__(self):
-        pass
+    def __init__(self, opinion_id, author, recommendation, stars, content, useful, useless, published, purchased, pros, cons):
+        self.opinion_id = opinion_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.published = published
+        self.purchased = purchased
+        self.pros = pros
+        self.cons = cons
--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,5 @@
 class Product():
-    def __init__(self):
-        pass
+    def __init__(self, product_id, stars, opinions):
+        self.product_id = product_id
+        self.stars = stars
+        self.opinions = opinions
